39130ae073d55921ee0ecd8cacb140823634a48f
acpasnello <anthony.pasnello@gmail.com>
2023-10-20 09:53:23 -0400

    Add placeToken, getToken, gameController functions

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ const gameboard = (() => {
         }
     }
 
+    const getBoard = () => board;
+
     function Cell() {
-        let value = 0;
+        let value = "";
 
         const addToken = (player) => {
             value = player;
@@ -23,16 +25,25 @@ const gameboard = (() => {
         return { addToken, getValue };
     }
 
-    function getTile(tile, selection) {
-        board[tile].innerText = selection;
+    function getTile(row, col) {
+        return board[row][col];
     }
-    const getBoard = () => board;
 
-    let playerOne = PlayerFactory("Player One", "x");
-    let playerTwo = PlayerFactory("Player Two", "o");
-    let players = [playerOne, playerTwo];
+    function placeToken(row, col, selection) {
+        if (board[row][col] == "") {
+            board[row][col] = selection;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    return { getTile, getBoard };
+    function printBoard () {
+        const boardWithValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithValues)
+    }
+
+    return { getTile, getBoard, placeToken, printBoard };
 })();
 
 // Player factory
@@ -42,14 +53,63 @@ const PlayerFactory = (name, token) => {
     function sayName() {
         console.log(this.name);
     }
+
+    function getToken () {
+        return this.token;
+    }
     // return public properties + functions
-    return { name, sayName };
+    return { name, getToken };
 };
 
-// DisplayController Module
-const displayController = (() => {
-    let board = gameboard.getBoard();
-    function updateTile(tile, selection) {
-        board[tile].innerText = selection;
+// Module to control the flow of the game
+const gameController = (() => {
+    const board = gameboard();
+
+    let playerOne = PlayerFactory("Player One", "x");
+    let playerTwo = PlayerFactory("Player Two", "o");
+    let players = [playerOne, playerTwo];
+
+    let activePlayer = players[0];
+
+    // Function to switch active player
+    const _changeActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        return activePlayer;
     }
+    
+    // Check for rows, diagonals, columns
+    const checkForWin = () => {
+        for (let i = 0; i < rows; i++) {
+            if (board.getTile(i, 0) === board.getTile(i, 1) === board.getTile(i, 2)) {
+                // Row
+                winner = board[i][0]
+            } else if ((i = 0 && (board[i][0] === board[i + 1][1]) === board[i + 2][2])) {
+                // Diagonal from top left
+                winner = board[i][0]
+            } else if ((i = 2 && (board[i][0] === board[i - 1][1]) === board[i - 2][2])) {
+                //Diagonal from bottom left
+                winner = board[i][0]
+            } else if (board)
+        }
+        return winner;
+    }   
+    
+    const playRound = (row, col) => {
+        // Check tile is empty
+        board.placeToken(row, col, activePlayer.getToken())
+        // Update Tile
+        // Check for line
+        checkForWin();
+        // Switch player
+        _changeActivePlayer();
+    }
+
+    
+
+    return {playRound}
+})();
+
+// DisplayController Module - control the display of the game
+const displayController = (() => {
+    
 })();
