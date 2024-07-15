b72db44dfbc94bd3f69887c63fd78447b72cf421
acpasnello <anthony.pasnello@gmail.com>
2024-07-15 18:08:16 -0400

    Fix typo in resetBoard

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const gameboard = (() => {
         for (let i = 0; i < rows; i++) {
             board[i] = [];
             for (let j = 0; j < columns; j++) {
-                board[i].clearCell();
+                board[i][j].clearCell();
             }
         }
     }
